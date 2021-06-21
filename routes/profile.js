cfc6ad94df969bd99a0bd04cb63a1e071af7efca
orianc <34635176+orianc@users.noreply.github.com>
2021-06-21 15:34:56 +0200

    Algo profileeditOK

--- routes/profile.js
+++ routes/profile.js
@@ -5,11 +5,61 @@ const collections = require('../collections');
 /**
  * get all post
  */
-router.get('/', async (req, res, next) => {
+// router.get('/get-profile', async (req, res, next) => {
+// 	try {
+// 		const USER_SESSION = await req.session.user;
+// 		const USER_ID = USER_SESSION._id;
+// 		console.log('userSession = ', USER_ID);
+// 		res.send(USER_SESSION);
+// 	} catch (error) {
+// 		console.error('fail to get session user : ', error);
+// 		res.status(500)('Error ');
+// 	}
+// });
+
+router.post('/edit', async (req, res, next) => {
 	try {
-		const USER_SESSION = await req.session.user;
-		console.log('userSession = ', USER_SESSION);
-		res.send(USER_SESSION);
+		const USER_SESSION_CHANGE = await req.body.user;
+		const USER_CURRENT_DB = collections.Users.findById(USER_SESSION_CHANGE._id, (err, user) => {
+			if (!user) {
+				req.flash('error', 'No Account');
+				return res.redirect('/login');
+			}
+
+			var email = USER_SESSION_CHANGE.email.trim();
+			var firstName = USER_SESSION_CHANGE.firstName.trim();
+			var lastName = USER_SESSION_CHANGE.lastName.trim();
+			var pw = USER_SESSION_CHANGE.pw.trim();
+			var age = USER_SESSION_CHANGE.age.trim();
+
+			user.email = email;
+			user.firstName = firstName;
+			user.lastName = lastName;
+			user.age = age;
+			user.pw = pw;
+
+			user.save((err) => {
+				res.redirect('http://localhost:3000/profile');
+			});
+		});
+		console.log('current change = ', USER_SESSION_CHANGE, 'and CURRENT DB INFO = ', USER_CURRENT_DB);
+
+		// const DB_USER_ID = collections.Users.find((user) => {
+		// 	user._id === USER_ID;
+		// });
+
+		// const newData = {
+		// 	_id: DB_USER_ID,
+		// 	firstName: (req.body.newDataUser.firstName || '').toString(),
+		// 	lastName: (req.body.newDataUser.lastName || '').toString(),
+		// 	email: (req.body.newDataUser.email || '').toString(),
+		// 	pw: (req.body.newDataUser.pw || '').toString(),
+		// };
+
+		// DB_USER_ID.console.log('userId = ', USER_ID);
+		// console.log('Find DB User = ', DB_USER_ID);
+
+		// res.send(USER_SESSION);
 	} catch (error) {
 		console.error('fail to get session user : ', error);
 		res.status(500)('Error ');
